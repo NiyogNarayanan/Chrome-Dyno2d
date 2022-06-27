23e0d439a97e863a47377995a76ba4330c6ff782
Niyog Narayanan <69643053+NiyogNarayanan@users.noreply.github.com>
2022-06-27 16:11:56 +0530

    Add files via upload

---
+++ Assets/Script/SpikeGenerator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SpikeGenerator : MonoBehaviour
+{
+    public GameObject spike;
+
+    public float MinSpeed;
+    public float MaxSpeed;
+    public float currentSpeed;
+
+    public float SpeedMuiltiplier;
+    // Awake is called before the first frame update
+    void Awake()
+    {
+        currentSpeed = MinSpeed;
+        generateSpike();
+    }
+
+    public void GenerateNextSpikeWithGap()
+    {
+        float randomWait = Random.Range(0.1f, 1.2f);
+        Invoke("generateSpike",randomWait);
+    }
+    public void generateSpike()
+    {
+      GameObject SpikeIns = Instantiate(spike, transform.position, transform.rotation);
+
+      SpikeIns.GetComponent<SpikeScript>().spikeGenerator = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentSpeed < MaxSpeed)
+        {
+            currentSpeed += SpeedMuiltiplier;
+        }
+    }
+}
